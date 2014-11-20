7dd93bceb21d42d06f276e43689ec0712fe4071b
AntaresOne <antaresone@outlook.com>
2014-11-20 13:47:16 +0100

    Fix typo

--- jf-common.mk
+++ jf-common.mk
@@ -109,7 +109,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     gps.msm8960
 
-#PRODUCT_COPY_FILES += \
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/gps.conf:/system/etc/gps.conf \
     $(LOCAL_PATH)/gps/etc/sap.conf:/system/etc/sap.conf
 
