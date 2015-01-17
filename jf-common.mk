8d2d2ae60234c85b2096edae5acaebc6ee5f9749
Spegelius <spegelius@gmail.com>
2015-01-17 17:01:39 +0200

    Fix 'Error while searching for networks'
    - current blobs return 6 elements per operator instead of the
      default 4
    - ro.ril.telephony.mqanelements=6 changes expected element count
      to 6
    
    Bug: CYAN-5764
    Change-Id: If2da02f87c5ebf701202f871c3a98aa6d4d04d2f
    
    Conflicts:
    	jf-common.mk

--- jf-common.mk
+++ jf-common.mk
@@ -270,7 +270,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.fill_eons=1 \
     persist.radio.use_se_table_only=1 \
     rild.libpath=/system/lib/libsec-ril.so \
-    ro.telephony.ril.config=newDriverCallU,newDialCode
+    ro.telephony.ril.config=newDriverCallU,newDialCode \
+    ro.ril.telephony.mqanelements=6
 
 # gps
 PRODUCT_PROPERTY_OVERRIDES += \
